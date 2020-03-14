c9d628a840155de3fa641dc3c507993508529dbd
Reza <rzgholizadeh@gmail.com>
2020-03-13 19:44:38 -0600

    chore: Add main project

---
+++ hateoas-backend/src/test/java/mrghz/hateoasbackend/HateoasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mrghz.hateoasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
